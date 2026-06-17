1f7af263f50f12f6dd1f442adf002d30deb76921
Percy Castro Gómez <percycastrogomez@gmail.com>
2026-06-17 00:09:57 -0500

    Update Blockchain.h

--- include/Blockchain.h
+++ include/Blockchain.h
@@ -7,14 +7,10 @@ class Blockchain {
 private:
     std::vector<Block> chain;
 
-    // Constructor privado para el patrón Singleton
     Blockchain();
 
 public:
-    // Singleton: devuelve la única instancia
     static Blockchain& getInstance();
-
-    // Deshabilitar copia y asignación
     Blockchain(const Blockchain&) = delete;
     Blockchain& operator=(const Blockchain&) = delete;
 
